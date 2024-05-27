cf082a6f243a13da9b18aa58df89a3de4d696a99
devmust <elmustapha.abourar@outlook.com>
2024-05-28 00:24:47 +0200

    clean up

--- hooks/use-viewer-token.ts
+++ hooks/use-viewer-token.ts
@@ -1,35 +1,43 @@
-import {useEffect, useState} from "react";
-import {toast} from "sonner";
-import {createViewerToken} from "@/actions/token";
-import {jwtDecode, JwtPayload} from "jwt-decode";
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import { JwtPayload, jwtDecode } from "jwt-decode";
+
+import { createViewerToken } from "@/actions/token";
 
 export const useViewerToken = (hostIdentity: string) => {
-    const [token , setToken] = useState("");
-    const [name , setName] = useState("");
-    const [identity , setIdentity] = useState("");
+    const [token, setToken] = useState("");
+    const [name, setName] = useState("");
+    const [identity, setIdentity] = useState("");
 
     useEffect(() => {
         const createToken = async () => {
             try {
-                const viewerToekn = await createViewerToken(hostIdentity);
-                setToken(viewerToekn);
+                const viewerToken = await createViewerToken(hostIdentity);
+                setToken(viewerToken);
 
-                const decodedToken = jwtDecode(viewerToekn) as JwtPayload & {name?: string};
-
+                const decodedToken = jwtDecode(viewerToken) as JwtPayload & { name?: string }
                 const name = decodedToken?.name;
-                const identity = decodedToken?.jti;
+                const identity = decodedToken.jti;
 
-                if (identity && name) {
+                if (identity) {
                     setIdentity(identity);
+                }
+
+                if (name) {
                     setName(name);
                 }
-            } catch (error) {
-                toast.success("Error creating token");
+
+            } catch {
+                toast.error("Something went wrong");
             }
         }
 
-        createToken()
+        createToken();
     }, [hostIdentity]);
 
-    return {token, name, identity};
-}
+    return {
+        token,
+        name,
+        identity,
+    };
+};
